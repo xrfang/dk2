23def8a963f91f3d7d0265d9cd5421caa810af4b
xrfang <xrfang@gmail.com>
2020-12-02 14:54:19 +0800

    WIP

--- ctrl/routes.go
+++ ctrl/routes.go
@@ -2,6 +2,7 @@ package ctrl
 
 import (
 	"net/http"
+	"path/filepath"
 )
 
 func notFound(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +18,7 @@ func setupRoutes(cf Config) {
 	http.HandleFunc("/dk/port/", apiScan)
 	http.HandleFunc("/dk/conn", notFound)
 	http.HandleFunc("/dk/conn/", apiConn)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(cf.WebRoot, "imgs/favicon.png"))
+	})
 }
--- verinfo.tpl
+++ verinfo.tpl
@@ -5,13 +5,9 @@ import "fmt"
 const (
 	_G_HASH = "{_G_HASH}"
 	_G_REVS = "{_G_REVS}"
-	_BRANCH = "{_BRANCH}"
 )
 
 func verinfo() string {
 	ver := fmt.Sprintf("V%s.%s", _G_REVS, _G_HASH)
-	if _BRANCH != "master" {
-		ver += "/" + _BRANCH
-	}
 	return ver
 }
